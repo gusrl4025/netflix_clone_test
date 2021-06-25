2cc7b1d50cae2c387348d97bf72b9ad0acb1fa88
dizzi <gusrl4025@gmail.com>
2021-06-25 15:39:37 +0900

    add CircleSlider, BoxSlider to HomeScreen/ HomeScreen UI finished

--- lib/screen/home_screen.dart
+++ lib/screen/home_screen.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:netflix_clone_test/model/model_movie.dart';
+import 'package:netflix_clone_test/widget/box_slider.dart';
 import 'package:netflix_clone_test/widget/carousel_slider.dart';
+import 'package:netflix_clone_test/widget/circle_slider.dart';
 
 class HomeScreen extends StatefulWidget {
   _HomeScreenState createState() => _HomeScreenState();
@@ -47,8 +49,10 @@ class _HomeScreenState extends State<HomeScreen> {
             movies: movies),
             TopBar(),
           ],
-        )
-      ]
+        ),
+        CircleSlider(movies: movies),
+        BoxSlider(movies: movies),
+      ],
     );
   }
 }
@@ -57,37 +61,38 @@ class TopBar extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-        padding: EdgeInsets.fromLTRB(20, 7, 20, 7),
-        child: Row(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          children: <Widget>[
-            Image.asset(
-              'images/bbongflix_logo.png',
-              fit: BoxFit.contain,
-              height: 25,
+      padding: EdgeInsets.fromLTRB(20, 7, 20, 7),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: <Widget>[
+          Image.asset(
+            'images/bbongflix_logo.png',
+            fit: BoxFit.contain,
+            height: 25,
+          ),
+          Container(
+            padding: EdgeInsets.only(right: 1),
+            child: Text(
+              'TV 프로그램',
+              style: TextStyle(fontSize: 14),
             ),
-            Container(
-              padding: EdgeInsets.only(right: 1),
-              child: Text(
-                'TV 프로그램',
-                style: TextStyle(fontSize: 14),
-              ),
+          ),
+          Container(
+            padding: EdgeInsets.only(right: 1),
+            child: Text(
+              '영화',
+              style: TextStyle(fontSize: 14),
             ),
-            Container(
-              padding: EdgeInsets.only(right: 1),
-              child: Text(
-                '영화',
-                style: TextStyle(fontSize: 14),
-              ),
+          ),
+          Container(
+            padding: EdgeInsets.only(right: 1),
+            child: Text(
+              '내가 찜한 콘텐츠',
+              style: TextStyle(fontSize: 14),
             ),
-            Container(
-              padding: EdgeInsets.only(right: 1),
-              child: Text(
-                '내가 찜한 콘텐츠',
-                style: TextStyle(fontSize: 14),
-              ),
-            ),
-          ],
-        ));
+          ),
+        ],
+      )
+    );
   }
 }
