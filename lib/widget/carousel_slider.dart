b51db9608a747e681bf6f46603e1a4065cdb81f4
dizzi <gusrl4025@gmail.com>
2021-06-28 05:31:27 +0900

    Auto stash before merge of "main" and "origin/main"

--- lib/widget/carousel_slider.dart
+++ lib/widget/carousel_slider.dart
@@ -1,8 +1,13 @@
+<<<<<<< Updated upstream
 import 'package:carousel_slider/carousel_options.dart';
 import 'package:carousel_slider/carousel_slider.dart';
 import 'package:flutter/material.dart';
 import 'package:netflix_clone_test/model/model_movie.dart';
 import 'package:netflix_clone_test/screen/detail_screen.dart';
+=======
+import 'package:flutter/material.dart';
+import 'package:netflix_clone_test/model/model_movie.dart';
+>>>>>>> Stashed changes
 
 class CarouselImage extends StatefulWidget {
   final List<Movie> movies;
@@ -17,7 +22,11 @@ class _CarouselImageState extends State<CarouselImage> {
   List<bool> likes;
   int _currentPage = 0;
   String _currentKeyword;
+<<<<<<< Updated upstream
   
+=======
+
+>>>>>>> Stashed changes
   @override
   void initState() {
     super.initState();
@@ -25,13 +34,17 @@ class _CarouselImageState extends State<CarouselImage> {
     images = movies.map((m) => Image.asset('./images/' + m.poster)).toList();
     keywords = movies.map((m) => m.keyword).toList();
     likes = movies.map((m) => m.like).toList();
+<<<<<<< Updated upstream
     _currentKeyword = keywords[0];
+=======
+>>>>>>> Stashed changes
   }
 
   @override
   Widget build(BuildContext context) {
     return Container(
       child: Column(
+<<<<<<< Updated upstream
         children: <Widget> [
           Container(
             padding: EdgeInsets.all(20),
@@ -135,10 +148,18 @@ class _CarouselImageState extends State<CarouselImage> {
             ),
           )
         ],
+=======
+        children: <Widget>[
+          Container(
+            padding:
+          )
+        ]
+>>>>>>> Stashed changes
       )
     );
   }
 }
+<<<<<<< Updated upstream
 
 List<Widget> makeIndicator(List list, int _currentPage) {
   List<Widget> results = [];
@@ -161,3 +182,5 @@ List<Widget> makeIndicator(List list, int _currentPage) {
 
   return results;
 }
+=======
+>>>>>>> Stashed changes
